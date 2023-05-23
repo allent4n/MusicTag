58f5753d8ab25367b18d7236b29c2242963e66ce
AllenTAN <78404109+allent4n@users.noreply.github.com>
2023-05-24 01:03:06 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -14,6 +14,7 @@ def args_parser():
     parser.add_argument("--data_path", type=str, default="1k_data", help="data path of the training data")
     parser.add_argument("--mode", type=str, default="train", help="train is training mode and test is testing mode")
     parser.add_argument("--model", type=str, default="cnn_lstm", help="selecting model")
+    parser.add_argument("--epochs", type=int, default=200, help="model training epochs")
     
     args = parser.parse_args()
     return args
